96e793839ffd4bfa97727ce88aff9b82076b448d
SANTHIYA R <santhiyar@SANTHIYAs-MacBook-Air.local>
2026-04-07 21:00:44 +0530

    final fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // ---- Middleware ----
 app.use(cors({
-  origin: "https://academic-stress-level-assessment-system-cpk8ze2j8.vercel.app",
+  origin: true,   // ✅ allows all Vercel deployments
   credentials: true
 }));
 app.use(express.json());
